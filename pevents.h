e4edb986c6acfb48e0d95b845bcdca75595f5308
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2011-10-12 23:05:58 -0500

    TBD: Add posix-styled functions for using neosmart_event_t objects

--- pevents.h
+++ pevents.h
@@ -5,12 +5,17 @@
 
 namespace neosmart
 {
+    //Type declarations
 	struct neosmart_event_t_;
 	typedef neosmart_event_t_ * neosmart_event_t;
 	
+    //WIN32-style pevent functions
 	neosmart_event_t CreateEvent(bool manualReset = false, bool initialState = false);
 	int DestroyEvent(neosmart_event_t event);
 	int WaitForEvent(neosmart_event_t event, uint32_t milliseconds = -1);
 	int SetEvent(neosmart_event_t event);
 	int ResetEvent(neosmart_event_t event);
+    
+    //posix-style functions
+    //TBD
 }
