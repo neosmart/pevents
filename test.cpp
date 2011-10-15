4036b081f6924420679f37a50a9a8abe562cb4b5
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2011-10-15 18:40:22 -0500

    Added assertions to test case and checking for incorrect index on return

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <pthread.h>
 #include <unistd.h>
+#include <assert.h>
 
 using namespace neosmart;
 using namespace std;
@@ -10,8 +11,11 @@ void *numbers(void *param);
 void *letters(void *param);
 
 neosmart_event_t event[2];
-char letter;
-int number;
+char letter = '?';
+int number = -1;
+
+char lastChar = 'Z';
+int lastNum = 100;
 
 void *letters(void *param)
 {
@@ -44,13 +48,28 @@ int main()
 	
 	for(int i = 0; i < 25; ++i)
 	{
-		int index;
+		int index = 7;
 		if(WaitForMultipleEvents(event, 2, false, -1, index) != 0)
+		{
 			cout << "Timeout!" << endl;
+		}
 		else if(index == 0)
+		{
+			assert(lastChar != letter);
 			cout << letter << endl;
+			lastChar = letter;
+		}
 		else if(index == 1)
+		{
+			assert(lastNum != number);
 			cout << number << endl;
+			lastNum = number;
+		}
+		else
+		{
+			cout << "ERROR! Index: " << index << endl;
+			exit(-1);
+		}
 	}
 	
 	DestroyEvent(event[0]);
